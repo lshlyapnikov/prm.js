9cab20fed2d6f5ebc366d0d26a8bbe54b10b38c2
lshlyapnikov <lshlyapnikov@gmail.com>
2014-02-19 23:25:38 -0500

    bumped up mochaTest timeout, yahoo finance api call takes ~6s

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,44 +6,45 @@
 /* global module */
 
 module.exports = function(grunt) {
-    var mochaTestGrep = grunt.option('mocha-test-grep') || '';
-    var mochaTestSrc = grunt.option('mocha-test-src') || 'src/test/server/*.js';
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+  var mochaTestGrep = grunt.option('mocha-test-grep') || '';
+  var mochaTestSrc = grunt.option('mocha-test-src') || 'src/test/server/*.js';
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-        jshint: {
-            files: ['Gruntfile.js', 'src/**/*.js']
-        },
+    jshint: {
+      files: ['Gruntfile.js', 'src/**/*.js']
+    },
 
-        mochaTest: {
-            test: {
-                options: {
-                    reporter: 'list',
-                    grep: mochaTestGrep
-                },
-                src: [mochaTestSrc]
-            }
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'list',
+          grep: mochaTestGrep,
+          timeout: '10s' /* yahoo api call takes about 6s */
         },
-        
-        watch: {
-            scripts: {
-                files: ['<%= jshint.files %>'],
-                tasks: ['jshint'],
-                options: {
-                    spawn: false,
-                    interrupt: true
-                }
-            }
+        src: [mochaTestSrc]
+      }
+    },
+
+    watch: {
+      scripts: {
+        files: ['<%= jshint.files %>'],
+        tasks: ['jshint'],
+        options: {
+          spawn: false,
+          interrupt: true
         }
-    });
+      }
+    }
+  });
 
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-mocha-test');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.registerTask('test', ['jshint', "mochaTest"]);
-    grunt.registerTask('default', ['jshint', 'mochaTest']);
-    grunt.registerTask('debug', 'Debug unit test', function() {
-        grunt.task.run('mochaTest');
-    });
+  grunt.registerTask('test', ['jshint', "mochaTest"]);
+  grunt.registerTask('default', ['jshint', 'mochaTest']);
+  grunt.registerTask('debug', 'Debug unit test', function() {
+    grunt.task.run('mochaTest');
+  });
 };
