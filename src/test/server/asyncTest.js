08151f4920edb59b3c221994ff985720bbd33cd6
lshlyapnikov <lshlyapnikov@gmail.com>
2014-02-22 23:16:39 -0500

    asyncTest

---
+++ src/test/server/asyncTest.js
@@ -0,0 +1,44 @@
+// http://jshint.com/docs/#config
+/* jshint strict: false */
+/* jshint undef: true */
+/* jshint unused: true */
+/* jshint browser: true */
+/* global require, describe, it */
+
+var async = require("../../main/server/async");
+var assert = require("assert");
+
+describe("async", function () {
+  describe("#assert", function () {
+    it("should call doneFunc with defined error when 1st is undefined", function () {
+      var doneError;
+
+      function doneFunction(error) {
+        doneError = error;
+      }
+
+      async.assert(undefined, doneFunction);
+      assert.ok(doneError !== undefined);
+    });
+    it("should call doneFunc with defined error when 1st is false", function () {
+      var doneError;
+
+      function doneFunction(error) {
+        doneError = error;
+      }
+
+      async.assert(false, doneFunction);
+      assert.ok(doneError !== undefined);
+    });
+    it("should NOT call doneFunc when 1st is true", function () {
+      var doneError;
+
+      function doneFunction(error) {
+        doneError = error;
+      }
+
+      async.assert(true, doneFunction);
+      assert.ok(doneError === undefined);
+    });
+  });
+});
