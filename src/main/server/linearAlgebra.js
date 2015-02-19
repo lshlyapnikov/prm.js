17b78581bb1423861e577aa4cf204b0279fbe1b7
lshlyapnikov <lshlyapnikov@gmail.com>
2015-02-19 01:51:59 -0500

    using underscore library

--- src/main/server/linearAlgebra.js
+++ src/main/server/linearAlgebra.js
@@ -13,27 +13,28 @@
 // if vector is passed instead of matrix or if matrix dimensions do not allow multiplication.
 
 var numeric = require("numeric")
+var _ = require("underscore")
 
 function dim(matrixMxN) {
-  if("undefined" === typeof matrixMxN) {
+  if(_.isUndefined(matrixMxN)) {
     throw new Error("InvalidArgument: argument matrixMxN is undefined")
   }
 
-  if (!Array.isArray(matrixMxN)) {
+  if (!_.isArray(matrixMxN)) {
     throw new Error("InvalidArgument: argument matrixMxN has to be an Array")
   }
 
   var m = matrixMxN.length
-  if("number" !== typeof m) {
+  if(!_.isNumber(m)) {
     throw new Error("InvalidArgument: argument matrixMxN does not have rows")
   }
 
-  if (!Array.isArray(matrixMxN[0])) {
+  if (!_.isArray(matrixMxN[0])) {
     throw new Error("InvalidArgument: matrixMxN is not a matrix")
   }
 
   var n = matrixMxN[0].length
-  if("number" !== typeof n) {
+  if(!_.isNumber(n)) {
     throw new Error("InvalidArgument: argument matrix does not have columns")
   }
 
@@ -41,10 +42,10 @@ function dim(matrixMxN) {
 }
 
 function matrix(m, n, initialValue) {
-  if("number" !== typeof m || m <= 0) {
+  if(!_.isNumber(m) || m <= 0) {
     throw new Error("InvalidArgument: invalid m: " + m)
   }
-  if("number" !== typeof n || n <= 0) {
+  if(!_.isNumber(n) || n <= 0) {
     throw new Error("InvalidArgument: invalid n: " + n)
   }
 
@@ -55,7 +56,7 @@ function matrix(m, n, initialValue) {
     result[i] = new Array(n)
   }
 
-  if(undefined !== initialValue) {
+  if(!_.isUndefined(initialValue)) {
     for(i = 0; i < m; i++) {
       for(j = 0; j < n; j++) {
         result[i][j] = initialValue
@@ -67,7 +68,7 @@ function matrix(m, n, initialValue) {
 }
 
 function rowMatrix(vector) {
-  if (!Array.isArray(vector)) {
+  if (!_.isArray(vector)) {
     throw new Error("InvalidArgument: argument vector has to be an Array")
   }
   return [vector]
@@ -96,11 +97,11 @@ function transpose(matrixMxN) {
 
 // TODO(lshlyapnikov) will be slow, C++ Node plugin??, map reduce?? 3rd party library???
 function multiplyMatrices(mXn, nXm) {
-  if(undefined === mXn) {
+  if(_.isUndefined(mXn)) {
     throw new Error("InvalidArgument: Argument mXn is undefined")
   }
 
-  if(undefined === nXm) {
+  if(_.isUndefined(nXm)) {
     throw new Error("InvalidArgument: Argument nXm is undefined")
   }
 
@@ -117,10 +118,10 @@ function multiplyMatrices(mXn, nXm) {
 function multiplyVectors(v0, v1) {
   var d0 = v0.length
   var d1 = v1.length
-  if (Array.isArray(v0[0])) {
+  if (_.isArray(v0[0])) {
     throw new Error("InvalidArgument: 1st argument has to be a vector")
   }
-  if (Array.isArray(v1[0])) {
+  if (_.isArray(v1[0])) {
     throw new Error("InvalidArgument: 2nd argument has to be a vector")
   }
   if (d0 !== d1) {
