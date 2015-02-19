865928c8ccd8a6bda5d3fefffeb415f51ccf8658
lshlyapnikov <lshlyapnikov@gmail.com>
2015-02-19 01:46:37 -0500

    removed semi-colons

--- src/main/server/linearAlgebra.js
+++ src/main/server/linearAlgebra.js
@@ -5,195 +5,194 @@
 /* jshint strict: false */
 /* jshint undef: true */
 /* jshint unused: true */
-/* jshint browser: true */
+/* jshint -W033 */
 /* global require, exports */
 
 // for some reason the current numeric.js implementation does not check matrix dimensions before multiplication.
 // numeric.dot() does too many assumptions/deductions -- don't really like it. Want my functions to throw
 // if vector is passed instead of matrix or if matrix dimensions do not allow multiplication.
 
-var numeric = require("numeric");
+var numeric = require("numeric")
 
 function dim(matrixMxN) {
   if("undefined" === typeof matrixMxN) {
-    throw new Error("InvalidArgument: argument matrixMxN is undefined");
+    throw new Error("InvalidArgument: argument matrixMxN is undefined")
   }
 
   if (!Array.isArray(matrixMxN)) {
-    throw new Error("InvalidArgument: argument matrixMxN has to be an Array");
+    throw new Error("InvalidArgument: argument matrixMxN has to be an Array")
   }
 
-  var m = matrixMxN.length;
+  var m = matrixMxN.length
   if("number" !== typeof m) {
-    throw new Error("InvalidArgument: argument matrixMxN does not have rows");
+    throw new Error("InvalidArgument: argument matrixMxN does not have rows")
   }
 
   if (!Array.isArray(matrixMxN[0])) {
-    throw new Error("InvalidArgument: matrixMxN is not a matrix");
+    throw new Error("InvalidArgument: matrixMxN is not a matrix")
   }
 
-  var n = matrixMxN[0].length;
+  var n = matrixMxN[0].length
   if("number" !== typeof n) {
-    throw new Error("InvalidArgument: argument matrix does not have columns");
+    throw new Error("InvalidArgument: argument matrix does not have columns")
   }
 
-  return [m, n];
+  return [m, n]
 }
 
 function matrix(m, n, initialValue) {
   if("number" !== typeof m || m <= 0) {
-    throw new Error("InvalidArgument: invalid m: " + m);
+    throw new Error("InvalidArgument: invalid m: " + m)
   }
   if("number" !== typeof n || n <= 0) {
-    throw new Error("InvalidArgument: invalid n: " + n);
+    throw new Error("InvalidArgument: invalid n: " + n)
   }
 
-  var result = new Array(m);
-  var i, j;
+  var result = new Array(m)
+  var i, j
 
   for(i = 0; i < m; i++) {
-    result[i] = new Array(n);
+    result[i] = new Array(n)
   }
 
   if(undefined !== initialValue) {
     for(i = 0; i < m; i++) {
       for(j = 0; j < n; j++) {
-        result[i][j] = initialValue;
+        result[i][j] = initialValue
       }
     }
   }
 
-  return result;
+  return result
 }
 
 function rowMatrix(vector) {
   if (!Array.isArray(vector)) {
-    throw new Error("InvalidArgument: argument vector has to be an Array");
+    throw new Error("InvalidArgument: argument vector has to be an Array")
   }
-  return [vector];
+  return [vector]
 }
 
 function columnMatrix(vector) {
-  return transpose(rowMatrix(vector));
+  return transpose(rowMatrix(vector))
 }
 
 // TODO(lshlyapnikov) will be slow, C++ Node plugin???
 function transpose(matrixMxN) {
-  var dimensions = dim(matrixMxN);
-  var m = dimensions[0];
-  var n = dimensions[1];
+  var dimensions = dim(matrixMxN)
+  var m = dimensions[0]
+  var n = dimensions[1]
 
-  var resultNxM = matrix(n, m);
-  var i, j;
+  var resultNxM = matrix(n, m)
+  var i, j
   for(i = 0; i < m; i++) {
     for(j = 0; j < n; j++) {
-      resultNxM[j][i] = matrixMxN[i][j];
+      resultNxM[j][i] = matrixMxN[i][j]
     }
   }
 
-  return resultNxM;
+  return resultNxM
 }
 
 // TODO(lshlyapnikov) will be slow, C++ Node plugin??, map reduce?? 3rd party library???
 function multiplyMatrices(mXn, nXm) {
   if(undefined === mXn) {
-    throw new Error("InvalidArgument: Argument mXn is undefined");
+    throw new Error("InvalidArgument: Argument mXn is undefined")
   }
 
   if(undefined === nXm) {
-    throw new Error("InvalidArgument: Argument nXm is undefined");
+    throw new Error("InvalidArgument: Argument nXm is undefined")
   }
 
-  var dim0 = dim(mXn);
-  var dim1 = dim(nXm);
+  var dim0 = dim(mXn)
+  var dim1 = dim(nXm)
 
   if(dim0[1] !== dim1[0]) {
-    throw new Error("InvalidArgument: " +
-      "Invalid matrix dimensions. Cannot multiply " + dim0 + " matrix by " + dim1);
+    throw new Error("InvalidArgument: Invalid matrix dimensions. Cannot multiply " + dim0 + " matrix by " + dim1)
   }
 
-  return numeric.sdotMM(mXn, nXm);
+  return numeric.sdotMM(mXn, nXm)
 }
 
 function multiplyVectors(v0, v1) {
-  var d0 = v0.length;
-  var d1 = v1.length;
+  var d0 = v0.length
+  var d1 = v1.length
   if (Array.isArray(v0[0])) {
-    throw new Error("InvalidArgument: 1st argument has to be a vector");
+    throw new Error("InvalidArgument: 1st argument has to be a vector")
   }
   if (Array.isArray(v1[0])) {
-    throw new Error("InvalidArgument: 2nd argument has to be a vector");
+    throw new Error("InvalidArgument: 2nd argument has to be a vector")
   }
   if (d0 !== d1) {
-    throw new Error("InvalidArgument: vectors have different dimensions: " + d0 + " and " + d1);
+    throw new Error("InvalidArgument: vectors have different dimensions: " + d0 + " and " + d1)
   }
-  return numeric.dotVV(v0, v1);
+  return numeric.dotVV(v0, v1)
 }
 
 function validateMatrix(mXn) {
-  var mn = dim(mXn);
-  var m = mn[0];
-  var n = mn[1];
+  var mn = dim(mXn)
+  var m = mn[0]
+  var n = mn[1]
 
   if(m === 0) {
-    throw new Error("InvalidArgument: matrix has 0 rows");
+    throw new Error("InvalidArgument: matrix has 0 rows")
   }
 
   if(n === 0) {
-    throw new Error("InvalidArgument: matrix has 0 columns");
+    throw new Error("InvalidArgument: matrix has 0 columns")
   }
 
-  var i, shouldBeN;
+  var i, shouldBeN
   for(i = 1; i < m; i++) {
-    shouldBeN = mXn[i].length;
+    shouldBeN = mXn[i].length
     if(shouldBeN !== n) {
-      throw new Error("InvalidArgument: expected " + n + " elements in row: " + i);
+      throw new Error("InvalidArgument: expected " + n + " elements in row: " + i)
     }
   }
 }
 
 exports.copyMatrix = function(mXn) {
-  validateMatrix(mXn);
-  var mn = dim(mXn);
-  var m = mn[0];
-  var n = mn[1];
+  validateMatrix(mXn)
+  var mn = dim(mXn)
+  var m = mn[0]
+  var n = mn[1]
 
-  var result = matrix(m, n, NaN);
-  var i, j;
+  var result = matrix(m, n, NaN)
+  var i, j
   for (i = 0; i < m; i++) {
     for (j = 0; j < n; j++) {
-      result[i][j] = Number(mXn[i][j]);
+      result[i][j] = Number(mXn[i][j])
     }
   }
 
-  return result;
-};
+  return result
+}
 
 exports.copyMatrixInto = function(mXn, outputMatrix) {
-  validateMatrix(mXn);
-  var mn = dim(mXn);
-  var m = mn[0];
-  var n = mn[1];
+  validateMatrix(mXn)
+  var mn = dim(mXn)
+  var m = mn[0]
+  var n = mn[1]
 
-  var i, j;
+  var i, j
   for (i = 0; i < m; i++) {
     for (j = 0; j < n; j++) {
-      outputMatrix[i][j] = Number(mXn[i][j]);
+      outputMatrix[i][j] = Number(mXn[i][j])
     }
   }
 
-  return outputMatrix;
-};
+  return outputMatrix
+}
 
 exports.inverseMatrix = function(mXn) {
-  return numeric.inv(mXn);
-};
+  return numeric.inv(mXn)
+}
 
-exports.dim = dim;
-exports.matrix = matrix;
-exports.rowMatrix = rowMatrix;
-exports.columnMatrix = columnMatrix;
-exports.transpose = transpose;
-exports.multiplyMatrices = multiplyMatrices;
-exports.multiplyVectors = multiplyVectors;
-exports.validateMatrix = validateMatrix;
+exports.dim = dim
+exports.matrix = matrix
+exports.rowMatrix = rowMatrix
+exports.columnMatrix = columnMatrix
+exports.transpose = transpose
+exports.multiplyMatrices = multiplyMatrices
+exports.multiplyVectors = multiplyVectors
+exports.validateMatrix = validateMatrix
