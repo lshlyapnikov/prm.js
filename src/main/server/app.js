46e508a3308759756e1813ef3a303ccc4708c9b1
lshlyapnikov <lshlyapnikov@gmail.com>
2015-06-25 22:36:49 -0400

    lambdas

--- src/main/server/app.js
+++ src/main/server/app.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
   res.json([symbols, startDate, endDate])
 })
 
-var server = app.listen(8080, function () {
+var server = app.listen(8080, () => {
   var host = server.address().address
   var port = server.address().port
   console.log('Example app listening at http://%s:%s', host, port)
