d53ec30b85bc0729de489ebf22fa0bb8c6a5c689
lshlyapnikov <lshlyapnikov@gmail.com>
2015-06-25 22:34:35 -0400

    expresjs server WIP

---
+++ src/main/server/app.js
@@ -0,0 +1,25 @@
+// http://jshint.com/docs/#config
+/* jshint strict: false */
+/* jshint undef: true */
+/* jshint unused: true */
+/* jshint browser: true */
+/* jshint -W033 */
+/* jshint -W119 */
+
+var express = require('express');
+var app = express();
+//var _ = require("underscore")
+
+app.get('/', (req, res) => {
+  console.log("query: " + JSON.stringify(req.query))
+  var symbols = req.query.symbols.split(",").map(s => s.trim())
+  var startDate = new Date(req.query.startDate)
+  var endDate = new Date(req.query.endDate)
+  res.json([symbols, startDate, endDate])
+})
+
+var server = app.listen(8080, function () {
+  var host = server.address().address
+  var port = server.address().port
+  console.log('Example app listening at http://%s:%s', host, port)
+})
