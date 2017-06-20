bcfe6b116a134fccd010ec63de02a20110f3fcd5
lshlyapnikov <lshlyapnikov@gmail.com>
2017-06-19 21:22:17 -0400

    fixing version and adding missing .jshintrc

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,19 @@
 // http://jshint.com/docs/#config
 /* global module */
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
   var mochaTestGrep = grunt.option('mocha-test-grep') || '';
   var mochaTestSrc = grunt.option('mocha-test-src') || 'src/test/*/*.js';
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
     jshint: {
-      files: ['Gruntfile.js', 'src/**/*.js', '!src/main/client/bower_components/**'],
+      files: [
+        'Gruntfile.js',
+        'src/**/*.js',
+        '!src/main/client/bower_components/**',
+        '!src/main/client-old/bower_components/**'
+      ],
       options: {
         jshintrc: true
       }
@@ -43,7 +48,7 @@ module.exports = function(grunt) {
 
   grunt.registerTask('test', ['jshint', "mochaTest"]);
   grunt.registerTask('default', ['jshint', 'mochaTest']);
-  grunt.registerTask('debug', 'Debug unit test', function() {
+  grunt.registerTask('debug', 'Debug unit test', function () {
     grunt.task.run('mochaTest');
   });
 };
