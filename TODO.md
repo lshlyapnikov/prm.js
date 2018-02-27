5be538d848625e73d8187fc41437cfcf7b918b0f
lshlyapnikov <lshlyapnikov@gmail.com>
2018-02-26 21:35:13 -0500

    new UI WIP

--- TODO.md
+++ TODO.md
@@ -1,2 +1,1 @@
 # TODO
-1. Need UI/end-to-end tests. Look at Protractor: https://angular.github.io/protractor/#/
---
+++ src/StocksForm.js
@@ -0,0 +1,45 @@
+// @flow
+import React, { Component } from 'react'
+
+import TextField from 'material-ui/TextField'
+import RaisedButton from 'material-ui/RaisedButton'
+import FloatingActionButton from 'material-ui/FloatingActionButton'
+import ContentAdd from 'material-ui/svg-icons/content/add'
+import ContentRemove from 'material-ui/svg-icons/content/remove'
+
+
+type Props = {
+  stock: string
+}
+
+type State = {
+  rows: Array<string>
+}
+
+class StockForm extends Component<Props, State> {
+  state = {
+    rows: []
+  }
+  render() {
+    return (
+      <div>
+        <TextField id="stock" hintText="Stock symbol" defaultValue={this.props.stock} />
+        &nbsp;
+        <FloatingActionButton mini={true} secondary={true}>
+          <ContentRemove />
+        </FloatingActionButton>
+        &nbsp;
+        <FloatingActionButton mini={true} primary={true}>
+          <ContentAdd />
+        </FloatingActionButton>
+        &nbsp;
+        <br />
+        <RaisedButton secondary={true}>Cancel</RaisedButton>
+        &nbsp;
+        <RaisedButton primary={true}>OK</RaisedButton>
+      </div>
+    );
+  }
+}
+
+export default StockForm
