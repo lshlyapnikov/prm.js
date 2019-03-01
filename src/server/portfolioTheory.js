bf7300881be8c3a74443c0b8d70940faa427346b
lshlyapnikov <lshlaypnikov@gmail.com>
2019-02-28 20:12:49 -0500

    more types

--- src/server/portfolioTheory.js
+++ src/server/portfolioTheory.js
@@ -1,98 +1,103 @@
 /// Author: Leonid Shlyapnikov
 /// LGPL Licencsed
-
-const la = require("./linearAlgebra")
-const pStats = require("./portfolioStats")
+// @flow strict
+import { type Matrix, matrix, dim, multiplyMatrices, transpose, copyMatrixInto } from "./linearAlgebra"
+import { type PortfolioStats, createPortfolioStats } from "./portfolioStats"
 const numeric = require("numeric")
-const _ = require("underscore")
 
-exports.GlobalMinimumVarianceEfficientPortfolio = {
+export class GlobalMinimumVarianceEfficientPortfolio {
   /**
    * Calculates Global Minimum Variance Portfolio.
    *
    * @param {Array.<Array.<number>>} expectedRrNx1    N x 1 expected/mean return rates matrix, where
    * @param {Array.<Array.<number>>} rrCovarianceNxN
    */
-  calculate: function (expectedRrNx1, rrCovarianceNxN) {
+  calculate(expectedRrNx1: Matrix<number>, rrCovarianceNxN: Matrix<number>): PortfolioStats {
     const weightsN = this.calculateWeightsFromReturnRatesCovariance(rrCovarianceNxN)
-    return pStats.createPortfolioStats(weightsN, expectedRrNx1, rrCovarianceNxN)
-  },
+    return createPortfolioStats(weightsN, expectedRrNx1, rrCovarianceNxN)
+  }
 
   /**
    * @param returnRatesCovarianceNxN
    * @returns {Array.<number>} an array of N elements. Every element is a stock weight in the portfolio.
    */
-  calculateWeightsFromReturnRatesCovariance: function (returnRatesCovarianceNxN) {
-    var n = la.dim(returnRatesCovarianceNxN)[0]
+  calculateWeightsFromReturnRatesCovariance(returnRatesCovarianceNxN: Matrix<number>): Array<number> {
+    var n = dim(returnRatesCovarianceNxN)[0]
     var b = n + 1
     var matrixA = this.createMatrixA(returnRatesCovarianceNxN)
     var matrixB = this.createMatrixB(b)
     var matrixZ = numeric.solve(matrixA, matrixB)
     return matrixZ.slice(0, b - 1)
-  },
+  }
 
-  createMatrixA: function (returnRatesCovarianceNxN) {
-    var n = la.dim(returnRatesCovarianceNxN)[0]
+  createMatrixA(returnRatesCovarianceNxN: Matrix<number>): Matrix<number> {
+    var n = dim(returnRatesCovarianceNxN)[0]
     var a = n + 1
     var twoBySigmaNxN = numeric.mul(2, returnRatesCovarianceNxN)
-    var matrixA = la.matrix(a, a, 0)
-    matrixA = la.copyMatrixInto(twoBySigmaNxN, matrixA)
+    var matrixA = matrix(a, a, 0)
+    matrixA = copyMatrixInto(twoBySigmaNxN, matrixA)
     var i
     for (i = 0; i < a - 1; i++) {
       matrixA[a - 1][i] = 1
       matrixA[i][a - 1] = 1
     }
     return matrixA
-  },
+  }
 
-  createMatrixB: function (b) {
-    var matrixB = la.matrix(b, 1, 0)
+  createMatrixB(b: number): Matrix<number> {
+    var matrixB = matrix(b, 1, 0)
     matrixB[b - 1][0] = 1
     return matrixB
   }
 }
 
-exports.TangencyPortfolio = {
+export const globalMinimumVarianceEfficientPortfolio = new GlobalMinimumVarianceEfficientPortfolio()
+
+export class TangencyPortfolio {
   /**
    * @param expectedReturnRatesNx1
    * @param returnRatesCovarianceNxN
    * @param riskFreeReturnRate
    * @returns {Array} an array of N elements. Every element is a stock weight in the portfolio.
    */
-  calculate: function (expectedReturnRatesNx1, returnRatesCovarianceNxN, riskFreeReturnRate) {
-    var n = la.dim(returnRatesCovarianceNxN)[0]
-    var returnRatesCovarianceInvertedNxN = numeric.inv(returnRatesCovarianceNxN)
-    var riskFreeReturnRateNx1 = la.matrix(n, 1, riskFreeReturnRate)
-    var muMinusRfNx1 = numeric.sub(expectedReturnRatesNx1, riskFreeReturnRateNx1)
-    var topNx1 = la.multiplyMatrices(returnRatesCovarianceInvertedNxN, muMinusRfNx1)
-    var one1xN = la.matrix(1, n, 1)
-    var bot1x1 = la.multiplyMatrices(one1xN, topNx1)
-    var resultNx1 = numeric.div(topNx1, bot1x1[0][0])
-    return la.transpose(resultNx1)[0]
+  calculate(expectedReturnRatesNx1: Matrix<number>, returnRatesCovarianceNxN: Matrix<number>,
+    riskFreeReturnRate: number): Array<number> {
+    const n = dim(returnRatesCovarianceNxN)[0]
+    const returnRatesCovarianceInvertedNxN = numeric.inv(returnRatesCovarianceNxN)
+    const riskFreeReturnRateNx1 = matrix(n, 1, riskFreeReturnRate)
+    const muMinusRfNx1 = numeric.sub(expectedReturnRatesNx1, riskFreeReturnRateNx1)
+    const topNx1 = multiplyMatrices(returnRatesCovarianceInvertedNxN, muMinusRfNx1)
+    const one1xN = matrix(1, n, 1)
+    const bot1x1 = multiplyMatrices(one1xN, topNx1)
+    const resultNx1 = numeric.div(topNx1, bot1x1[0][0])
+    return transpose(resultNx1)[0]
   }
 }
+
+export const tangencyPortfolio = new TangencyPortfolio()
 
 /**
  * See econ424/08.2 portfolioTheoryMatrix.pdf, p12, matrix formula 1.18.
  * A and B matrices are different from global minimum efficient portfolio.
  */
-exports.TargetReturnEfficientPortfolio = {
+export class TargetReturnEfficientPortfolio {
 
-  calculate: function (expectedReturnRatesNx1, returnRatesCovarianceNxN, targetReturnRate) {
-    var n = la.dim(returnRatesCovarianceNxN)[0]
-    var b = n + 2
-    var matrixA = this.createMatrixA(expectedReturnRatesNx1, returnRatesCovarianceNxN)
-    var matrixB = this.createMatrixB(b, targetReturnRate)
-    var matrixZ = numeric.solve(matrixA, matrixB)
+  calculate(expectedReturnRatesNx1: Matrix<number>, returnRatesCovarianceNxN: Matrix<number>,
+    targetReturnRate: number): Array<number> {
+    const n = dim(returnRatesCovarianceNxN)[0]
+    const b = n + 2
+    const matrixA = this.createMatrixA(expectedReturnRatesNx1, returnRatesCovarianceNxN)
+    const matrixB = this.createMatrixB(b, targetReturnRate)
+    const matrixZ = numeric.solve(matrixA, matrixB)
     return matrixZ.slice(0, b - 2)
-  },
+  }
 
-  createMatrixA: function (expectedReturnRatesNx1, returnRatesCovarianceNxN) {
-    var n = la.dim(returnRatesCovarianceNxN)[0]
+  createMatrixA(expectedReturnRatesNx1: Matrix<number>, returnRatesCovarianceNxN: Matrix<number>): Matrix<number> {
+    var n = dim(returnRatesCovarianceNxN)[0]
     var a = n + 2
     var twoBySigmaNxN = numeric.mul(2, returnRatesCovarianceNxN)
-    var matrixA = la.matrix(a, a, 0)
-    matrixA = la.copyMatrixInto(twoBySigmaNxN, matrixA)
+    var matrixA = matrix(a, a, 0)
+    matrixA = copyMatrixInto(twoBySigmaNxN, matrixA)
     var i
     for (i = 0; i < n; i++) {
       var r = expectedReturnRatesNx1[i][0]
@@ -104,53 +109,57 @@ exports.TargetReturnEfficientPortfolio = {
       matrixA[i][n + 1] = 1
     }
     return matrixA
-  },
+  }
 
-  createMatrixB: function (b, targetReturnRate) {
-    var matrixB = la.matrix(b, 1, 0)
+  createMatrixB(b: number, targetReturnRate: number) {
+    const matrixB = matrix(b, 1, 0)
     matrixB[b - 2][0] = targetReturnRate
     matrixB[b - 1][0] = 1
     return matrixB
   }
 }
 
+export const targetReturnEfficientPortfolio = new TargetReturnEfficientPortfolio()
+
 /**
  * econ424/08.2 portfolioTheoryMatrix.pdf, p.21
  */
-exports.EfficientPortfolioFrontier = {
-  calculate: function (expectedRrNx1, rrCovarianceNxN) {
-    var maxExpectedRr = _.max(expectedRrNx1, function (row) {
-      return row[0]
-    })
+export class EfficientPortfolioFrontier {
+  calculate(expectedRrNx1: Matrix<number>, rrCovarianceNxN: Matrix<number>): Array<PortfolioStats> {
+    const arr: Array<number> = expectedRrNx1.map(row => row[0])
+    const maxExpectedRr: number = Math.max(...arr)
 
-    var globalMinVarianceEp = pStats.createPortfolioStats(
-      exports.GlobalMinimumVarianceEfficientPortfolio.calculateWeightsFromReturnRatesCovariance(rrCovarianceNxN),
+    const globalMinVarianceEp = createPortfolioStats(
+      globalMinimumVarianceEfficientPortfolio.calculateWeightsFromReturnRatesCovariance(rrCovarianceNxN),
       expectedRrNx1,
       rrCovarianceNxN)
 
-    var maxReturnEp = pStats.createPortfolioStats(
-      exports.TargetReturnEfficientPortfolio.calculate(expectedRrNx1, rrCovarianceNxN, maxExpectedRr),
+    const maxReturnEp = createPortfolioStats(
+      targetReturnEfficientPortfolio.calculate(expectedRrNx1, rrCovarianceNxN, maxExpectedRr),
       expectedRrNx1,
       rrCovarianceNxN)
 
-    var maxNum = 21 // TODO: why 21? That is the number from the lecture
-    var result = new Array(maxNum)
+    const maxNum = 21 // TODO: why 21? That is the number from the lecture... just a number of iterations?
+    const result: Array<PortfolioStats> = new Array(maxNum)
 
-    for (var i = 0, alpha = 1; i < maxNum; i++, alpha -= 0.1) {
-      result[i] = pStats.createPortfolioStats(
+    for (let i = 0, alpha = 1; i < maxNum; i++, alpha -= 0.1) {
+      result[i] = createPortfolioStats(
         this._calculateEfficientPortfolioWeights(globalMinVarianceEp.weights, maxReturnEp.weights, alpha),
         expectedRrNx1,
         rrCovarianceNxN)
     }
 
     return result
-  },
+  }
 
-  _calculateEfficientPortfolioWeights: function (globalMinVarianceEpWeigthsN, maxReturnEpWeightsN, alpha) {
-    var x = _.map(globalMinVarianceEpWeigthsN, (n) => alpha * n)
-    var y = _.map(maxReturnEpWeightsN, (n) => (1 - alpha) * n)
+  _calculateEfficientPortfolioWeights(globalMinVarianceEpWeigthsN: Array<number>, maxReturnEpWeightsN: Array<number>,
+    alpha: number) {
+    const x = globalMinVarianceEpWeigthsN.map(n => alpha * n)
+    const y = maxReturnEpWeightsN.map(n => (1 - alpha) * n)
     return numeric.add(x, y)
   }
 }
+
+export const efficientPortfolioFrontier = new EfficientPortfolioFrontier()
 
 // TODO efficient portfolio frontier with no short sale: 09 portfoliotheorynoshortsalesslides.pdf, page 12
--- src/server/portfolioTheory.test.js
+++ src/server/portfolioTheory.test.js
@@ -1,113 +1,109 @@
+// @flow strict
 /* global describe, it */
 
-const pStats = require("./portfolioStats")
-const pTheory = require("./portfolioTheory")
-const la = require("./linearAlgebra")
-const utils = require("./utils")
-const testData = require("./testData")
-const assert = require("assert")
-const numeric = require("numeric")
+import * as assert from "assert"
+import * as numeric from "numeric"
+
+import {
+  PortfolioStats, portfolioStdDev, calculateReturnRatesFromPriceMatrix,
+  covariance, mean
+} from "./portfolioStats"
+import { type Matrix, columnMatrix, matrix, multiplyMatrices, transpose, dim } from "./linearAlgebra"
+import * as pTheory from "./portfolioTheory"
+import * as utils from "./utils"
+import * as testData from "./testData"
 
 const log = utils.logger("portfolioTheoryTest")
 
-describe("pTheory", function() {
+describe("pTheory", function () {
   // numbers taken from econ424/08.2 portfolioTheoryMatrix.pdf, p. 4, example 2
   // MSFT, NORD, SBUX
 
-  const expectedRr3x1 = la.columnMatrix([0.0427, 0.0015, 0.0285])
+  const expectedRr3x1: Matrix<number> = columnMatrix([0.0427, 0.0015, 0.0285])
 
-  const rrCovariance3x3 = [
+  const rrCovariance3x3: Matrix<number> = [
     [0.0100, 0.0018, 0.0011],
     [0.0018, 0.0109, 0.0026],
     [0.0011, 0.0026, 0.0199]
   ]
 
-  const riskFreeRr = 0.005
-
-  const globalMinVariancePortfolio = Object.create(pStats.PortfolioStats)
-  globalMinVariancePortfolio.weights = [0.4411, 0.3656, 0.1933]
-  globalMinVariancePortfolio.expectedReturnRate = 0.02489184
-  globalMinVariancePortfolio.stdDev = 0.07267607
+  const riskFreeRr: number = 0.005
+  const globalMinVariancePortfolio = new PortfolioStats([0.4411, 0.3656, 0.1933], 0.07267607, 0.02489184)
 
   describe("Global Minimum Variance Portfolio", () => {
     it("should calculate global min variance portfolio from return rate covariance matrix", () => {
       const actualWeights =
-        pTheory.GlobalMinimumVarianceEfficientPortfolio.calculateWeightsFromReturnRatesCovariance(rrCovariance3x3)
+        pTheory.globalMinimumVarianceEfficientPortfolio.calculateWeightsFromReturnRatesCovariance(rrCovariance3x3)
       log.debug("actualWeights: " + numeric.prettyPrint(actualWeights) + "\n")
       assert.deepEqual(numeric.dim(actualWeights), [3])
       utils.setArrayElementsScale(actualWeights, 4)
       assert.deepEqual(actualWeights, globalMinVariancePortfolio.weights)
 
       var actualWeights1x3 = [actualWeights]
-      var actualStdDev = pStats.portfolioStdDev(actualWeights1x3, rrCovariance3x3)
+      var actualStdDev = portfolioStdDev(actualWeights1x3, rrCovariance3x3)
       log.debug("actualStdDev: " + actualStdDev)
       assert.equal(actualStdDev.toFixed(8), globalMinVariancePortfolio.stdDev)
 
-      var portfolioRr = la.multiplyMatrices(actualWeights1x3, expectedRr3x1)
+      var portfolioRr = multiplyMatrices(actualWeights1x3, expectedRr3x1)
       assert.equal(portfolioRr[0][0].toFixed(4), globalMinVariancePortfolio.expectedReturnRate.toFixed(4))
     })
-    it("should calculate global min variance portfolio for NYX and INTC using historic prices", function() {
-      var expectedPortfolio = Object.create(pStats.PortfolioStats)
-      expectedPortfolio.weights = [0.1127, 0.8873]
-      expectedPortfolio.expectedReturnRate = 0.0064
-      expectedPortfolio.stdDev = 0.0737
+    it("should calculate global min variance portfolio for NYX and INTC using historic prices", function () {
+      const expectedPortfolio = new PortfolioStats([0.1127, 0.8873], 0.0737, 0.0064)
+      const priceMatrixMxN = transpose([testData.NYX, testData.INTC])
+      const returnRatesKxN = calculateReturnRatesFromPriceMatrix(priceMatrixMxN)
+      const expectedRrNx1 = mean(returnRatesKxN)
+      const rrCovarianceNxN = covariance(returnRatesKxN)
 
-      const priceMatrixMxN = la.transpose([testData.NYX, testData.INTC])
-      const returnRatesKxN = pStats.calculateReturnRatesFromPriceMatrix(priceMatrixMxN)
-      const expectedRrNx1 = pStats.mean(returnRatesKxN)
-      const rrCovarianceNxN = pStats.covariance(returnRatesKxN)
-
-      var actualPortfolio = pTheory.GlobalMinimumVarianceEfficientPortfolio.calculate(expectedRrNx1, rrCovarianceNxN)
+      var actualPortfolio = pTheory.globalMinimumVarianceEfficientPortfolio.calculate(expectedRrNx1, rrCovarianceNxN)
 
       assert.deepEqual(utils.newArrayWithScale(actualPortfolio.weights, 4), expectedPortfolio.weights)
       assert.equal(actualPortfolio.expectedReturnRate.toFixed(4), expectedPortfolio.expectedReturnRate)
       assert.equal(actualPortfolio.stdDev.toFixed(4), expectedPortfolio.stdDev)
     })
-    describe("#createMatrixA", function() {
+    describe("#createMatrixA", function () {
       function testCreateMatrixA(rrCov) {
-        var n = la.dim(rrCov)[0]
+        var n = dim(rrCov)[0]
         var a = n + 1
 
-        var matrixA = pTheory.GlobalMinimumVarianceEfficientPortfolio.createMatrixA(rrCov)
+        var matrixA = pTheory.globalMinimumVarianceEfficientPortfolio.createMatrixA(rrCov)
 
         log.debug("matrixA: \n" + numeric.prettyPrint(matrixA) + "\n")
 
-        assert.deepEqual(la.dim(matrixA), [a, a])
+        assert.deepEqual(dim(matrixA), [a, a])
         var i, j
-        for(i = 0; i < n; i++) {
-          for(j = 0; j < n; j++) {
+        for (i = 0; i < n; i++) {
+          for (j = 0; j < n; j++) {
             assert.equal(matrixA[i][j], 2 * rrCov[i][j])
           }
         }
-        for(i = 0; i < a - 1; i++) {
+        for (i = 0; i < a - 1; i++) {
           assert.equal(matrixA[a - 1][i], 1)
           assert.equal(matrixA[i][a - 1], 1)
         }
         assert.equal(matrixA[a - 1][a - 1], 0)
       }
 
-      it("should create 4x4 A matrix from 3x3 return rate covariance matrix", function() {
+      it("should create 4x4 A matrix from 3x3 return rate covariance matrix", function () {
         testCreateMatrixA(numeric.random([3, 3]))
       })
-      it("should create 5x5 A matrix from 4x4 return rate covariance matrix", function() {
+      it("should create 5x5 A matrix from 4x4 return rate covariance matrix", function () {
         testCreateMatrixA(numeric.random([4, 4]))
       })
-      it("should create 10x10 A matrix from 9x9 return rate covariance matrix", function() {
+      it("should create 10x10 A matrix from 9x9 return rate covariance matrix", function () {
         testCreateMatrixA(numeric.random([9, 9]))
       })
     })
   })
-  describe("Tangency Portfolio", function() {
-    it("should calculate tangency portfolio from return rate covariance matrix", function() {
+  describe("Tangency Portfolio", function () {
+    it("should calculate tangency portfolio from return rate covariance matrix", function () {
       var expectedTangencyPortfolioWeights = [1.0268, -0.3263, 0.2994]
-      var tangencyPortfolio = Object.create(pTheory.TangencyPortfolio)
-      var actualWeights = tangencyPortfolio.calculate(expectedRr3x1, rrCovariance3x3, riskFreeRr)
+      var actualWeights = pTheory.tangencyPortfolio.calculate(expectedRr3x1, rrCovariance3x3, riskFreeRr)
       assert.deepEqual(utils.newArrayWithScale(actualWeights, 4), expectedTangencyPortfolioWeights)
     })
   })
-  describe("Efficient Portfolio with Target Return", function() {
-    it("should create expected matrix A", function() {
-      const expectedRr3x1 = la.columnMatrix([1, 2, 3])
+  describe("Efficient Portfolio with Target Return", function () {
+    it("should create expected matrix A", function () {
+      const expectedRr3x1 = columnMatrix([1, 2, 3])
       var rrCovMatrix3x3 = [
         [10, 11, 12],
         [20, 21, 22],
@@ -117,50 +113,50 @@ describe("pTheory", function() {
         [20, 22, 24, 1, 1],
         [40, 42, 44, 2, 1],
         [60, 62, 64, 3, 1],
-        [ 1, 2, 3, 0, 0],
-        [ 1, 1, 1, 0, 0]
+        [1, 2, 3, 0, 0],
+        [1, 1, 1, 0, 0]
       ]
-      var actualMatrixA = pTheory.TargetReturnEfficientPortfolio.createMatrixA(expectedRr3x1, rrCovMatrix3x3)
+      var actualMatrixA = pTheory.targetReturnEfficientPortfolio.createMatrixA(expectedRr3x1, rrCovMatrix3x3)
       assert.deepEqual(actualMatrixA, expectedMatrixA)
     })
-    it("should create expected matrix B", function() {
-      var actualMatrixB = pTheory.TargetReturnEfficientPortfolio.createMatrixB(5, 10)
-      assert.deepEqual(actualMatrixB, la.columnMatrix([0, 0, 0, 10, 1]))
+    it("should create expected matrix B", function () {
+      const actualMatrixB = pTheory.targetReturnEfficientPortfolio.createMatrixB(5, 10)
+      assert.deepEqual(actualMatrixB, columnMatrix([0, 0, 0, 10, 1]))
     })
-    it("should calculate efficient portfolio with the same expected return as Microsoft", function() {
+    it("should calculate efficient portfolio with the same expected return as Microsoft", function () {
       // econ424/08.2 portfolioTheoryMatrix.pdf, p. 13, example 6
-      var expectedWeights = [0.82745, -0.09075, 0.26329]
+      const expectedWeights = [0.82745, -0.09075, 0.26329]
 
-      var actualWeights = pTheory.TargetReturnEfficientPortfolio.calculate(
+      const actualWeights = pTheory.targetReturnEfficientPortfolio.calculate(
         expectedRr3x1, rrCovariance3x3, expectedRr3x1[0][0])
 
       assert.deepEqual(utils.newArrayWithScale(actualWeights, 5), expectedWeights)
     })
-    it("should calculate efficient portfolio with the same expected return as Starbucks", function() {
+    it("should calculate efficient portfolio with the same expected return as Starbucks", function () {
       // econ424/08.2 portfolioTheoryMatrix.pdf, p. 14, example 7
-      var expectedWeights = [0.5194, 0.2732, 0.2075]
+      const expectedWeights = [0.5194, 0.2732, 0.2075]
 
-      var actualWeights = pTheory.TargetReturnEfficientPortfolio.calculate(
+      const actualWeights = pTheory.targetReturnEfficientPortfolio.calculate(
         expectedRr3x1, rrCovariance3x3, expectedRr3x1[2][0])
 
       assert.deepEqual(utils.newArrayWithScale(actualWeights, 4), expectedWeights)
     })
   })
-  describe("Efficient Portfolio Frontier", function() {
-    it("should calculate frontier for the lecture example", function() {
-      var actualFrontier = pTheory.EfficientPortfolioFrontier.calculate(expectedRr3x1, rrCovariance3x3)
+  describe("Efficient Portfolio Frontier", function () {
+    it("should calculate frontier for the lecture example", function () {
+      const actualFrontier = pTheory.efficientPortfolioFrontier.calculate(expectedRr3x1, rrCovariance3x3)
       assert.equal(21, actualFrontier.length)
       log.debug(numeric.prettyPrint(actualFrontier))
       assert.deepEqual(
         utils.newArrayWithScale(actualFrontier[0].weights, 4),
         globalMinVariancePortfolio.weights)
     })
-    it("should calculate frontier for NYX and INTC", function() {
-      const priceMatrixMxN = la.transpose([testData.NYX, testData.INTC])
-      const returnRatesKxN = pStats.calculateReturnRatesFromPriceMatrix(priceMatrixMxN)
-      const expectedRrNx1 = pStats.mean(returnRatesKxN)
-      const rrCovarianceNxN = pStats.covariance(returnRatesKxN)
-      const frontier = pTheory.EfficientPortfolioFrontier.calculate(expectedRrNx1, rrCovarianceNxN)
+    it("should calculate frontier for NYX and INTC", function () {
+      const priceMatrixMxN = transpose([testData.NYX, testData.INTC])
+      const returnRatesKxN = calculateReturnRatesFromPriceMatrix(priceMatrixMxN)
+      const expectedRrNx1 = mean(returnRatesKxN)
+      const rrCovarianceNxN = covariance(returnRatesKxN)
+      const frontier = pTheory.efficientPortfolioFrontier.calculate(expectedRrNx1, rrCovarianceNxN)
       log.debug(numeric.prettyPrint(frontier))
       assert.deepEqual(
         utils.newArrayWithScale(frontier[0].weights, 4),
