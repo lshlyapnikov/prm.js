06ae5c5c2a61ce2aee815e937014ea781d38ed0b
Leonid Shlyapnikov <lshlyapnikov@pop-os.localdomain>
2020-11-22 23:01:33 -0500

    wip

--- src/server/priceMatrixSafe.js
+++ src/server/priceMatrixSafe.js
@@ -5,7 +5,7 @@ import { type Result, equalArrays } from "./utils"
 
 export type SymbolPrices = {|
   +symbol: string,
-  +prices: Array<number>
+  +prices: $ReadOnlyArray<number>
 |}
 
 export function symbolPrices(symbol: string, prices: Array<number>): SymbolPrices {
@@ -19,18 +19,18 @@ export function maxPriceArrayLength(arr: Array<SymbolPrices>): number {
 
 export function createPriceMatrix<M: number, N: number>(
   symbols: Vector<N, string>,
-  symbolPrices: Array<SymbolPrices>,
-  m: M
+  symbolPrices: Vector<M, SymbolPrices>
 ): Result<Matrix<M, N, number>> {
+  const m: M = symbolPrices.n
+  const n: N = symbols.n
   if (m <= 0) {
-    const badSymbols: $ReadOnlyArray<string> = symbols.values
-    const error = new Error(
-      `Cannot build a price matrix. m: ${m}. No prices loaded for all provided symbols: ${JSON.stringify(badSymbols)}.`
-    )
-    return { success: false, error }
+    return { success: false, error: new Error(`Cannot build a price matrix. m: ${m}.`) }
+  }
+  if (n <= 0) {
+    return { success: false, error: new Error(`Cannot build a price matrix. n: ${n}.`) }
   }
 
-  const invalidPrices: $ReadOnlyArray<SymbolPrices> = findInvalidPriceArray(symbolPrices, m)
+  const invalidPrices: $ReadOnlyArray<SymbolPrices> = findInvalidPriceArray(symbolPrices)
 
   if (invalidPrices.length > 0) {
     const badSymbols: Array<string> = invalidPrices.map((p) => p.symbol)
@@ -41,7 +41,7 @@ export function createPriceMatrix<M: number, N: number>(
     return { success: false, error }
   }
 
-  const symbols2: Array<string> = symbolPrices.map((p) => p.symbol)
+  const symbols2: $ReadOnlyArray<string> = symbolPrices.values.map((p) => p.symbol)
   if (!equalArrays(symbols.values, symbols2)) {
     const error = new Error(
       `Wrong order: ${JSON.stringify(symbols.values)} must be equal to ${JSON.stringify(symbols2)}`
@@ -49,8 +49,7 @@ export function createPriceMatrix<M: number, N: number>(
     return { success: false, error }
   }
 
-  const n: N = symbols.n
-  const nXm: Array<Array<number>> = new Array(n)
+  const nXm: Array<$ReadOnlyArray<number>> = new Array(n)
 
   for (let i = 0; i < n; i++) {
     const priceArray: SymbolPrices = symbolPrices[i]
@@ -62,10 +61,8 @@ export function createPriceMatrix<M: number, N: number>(
 }
 
 // returns Array of invalid Prices or empty Array
-function findInvalidPriceArray(
-  array: $ReadOnlyArray<SymbolPrices>,
-  expectedLength: number
-): $ReadOnlyArray<SymbolPrices> {
+function findInvalidPriceArray<N: number>(array: Vector<N, SymbolPrices>): $ReadOnlyArray<SymbolPrices> {
+  const expectedLength: N = array.n
   function collectInvalidPrices(z: $ReadOnlyArray<SymbolPrices>, p: SymbolPrices): $ReadOnlyArray<SymbolPrices> {
     if (p.prices.length != expectedLength) {
       return z.concat(p)
@@ -73,8 +70,6 @@ function findInvalidPriceArray(
       return z
     }
   }
-
-  const invalidPrices: $ReadOnlyArray<SymbolPrices> = array.reduce(collectInvalidPrices, [])
-
+  const invalidPrices: $ReadOnlyArray<SymbolPrices> = array.values.reduce(collectInvalidPrices, [])
   return invalidPrices
 }
